8b84876e73640666c67198283856d80f45b37ef0
Amodhx <amxdhnanditha@gmail.com>
2025-05-07 14:40:52 +0530

    created revenue page

--- src/services/revenue.js
+++ src/services/revenue.js
@@ -1,1 +1,13 @@
 //Todo : Create Revenue api calls
+
+import ApiService from "./apiService"
+
+export async function getAllAndFilterRevenue(page, pageSize, obj) {
+    const apiObject = {}
+    apiObject.method = "POST"
+    apiObject.authentication = true
+    apiObject.endpoint = `admin/tokens/${page}/${pageSize}`
+    apiObject.body = obj
+    apiObject.notGroup = true
+    return await ApiService.callApi(apiObject)
+}
--- src/views/pages/revenue/index.js
+++ src/views/pages/revenue/index.js
@@ -1,78 +1,86 @@
 import React, {useEffect, useState} from "react"
-import {Button, Card, CardBody, Col, Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Row, Table} from "reactstrap"
+import {Card, CardBody, Table} from "reactstrap"
 import {useDispatch} from "react-redux"
 import {setLoading} from "../../../redux/actions/loading"
 import Pagination from "../../components/pagination"
 import {toast} from "react-toastify"
 import {useHistory} from "react-router-dom/cjs/react-router-dom"
-import Flatpickr from "react-flatpickr"
 import "flatpickr/dist/flatpickr.css"
 import DropDownContainer from "../../components/dropDownContainer/DropDownContainer"
+import {getAllAndFilterRevenue} from "../../../services/revenue"
 
 export default function Payout({ direction, ...args }) {
     const dispatch = useDispatch()
     const history = useHistory()
 
     const [data, setData] = useState([])
-    const [currentPage, setCurrentPage] = useState(0)
-    const [totalRecodes, setTotalRecodes] = useState(0)
+    const [dataType, setDataType] = useState('h_24')
+    const [startDate, setStartDate] = useState(null)
+    const [endDate, setEndDate] = useState(null)
+    const [currentPage, setCurrentPage] = useState(1)
     const [totalPages, setTotalPages] = useState(0)
-    const [selectedUser, setSelectedUser] = useState(null)
-    const [userLoading, setUserLoading] = useState(null)
-    const [search, setSearch] = useState("")
-    const [status, setStatus] = useState("pending")
-    const [start, setFromDate] = useState()
-    const [end, setToDate] = useState()
-    const [userId, setUserId] = useState()
-    const [type, setType] = useState("all")
 
-    const [dropdownOpen, setDropdownOpen] = useState(false)
+    const getDataHandler = async (page = 1, dataType, startDate, endDate) => {
+        setDataType(dataType)
+        setStartDate(startDate)
+        setEndDate(endDate)
+        if (dataType === "custom" && (!startDate || !endDate)) {
+            return // Stop execution if startDate or endDate is missing
+        }
+        dispatch(setLoading(true))
 
-    const toggle = () => setDropdownOpen((prevState) => !prevState)
+        let objToGetData
+        if (dataType === "custom") {
+            objToGetData = {
+                start: startDate,
+                end: endDate,
+                dateTypes: dataType
+            }
+        } else {
+            objToGetData = {
+                dateTypes: dataType
+            }
+        }
 
-    const getDataHandler = async (page = 1) => {
-        // // if (start && end) {
-        // dispatch(setLoading(true))
-        // await getAllPayouts(page - 1, 10, { date: start, type })
-        //     .then((res) => {
-        //         if (res.success) {
-        //             setData(res ?? [])
-        //             setTotalPages(res?.totalPages ?? 0)
-        //             // } else if (res.result) {
-        //             //     setData([])
-        //             //     setTotalPages(0)
-        //             //     toast.error("Please enter valid date range")
-        //         } else {
-        //             setData([])
-        //             setTotalPages(0)
-        //             toast.error("Something went wrong")
-        //         }
-        //     })
-        //     .finally(() => {
-        //         setCurrentPage(page)
-        //         dispatch(setLoading(false))
-        //     })
+        try {
+            const res = await getAllAndFilterRevenue(page - 1, 10, objToGetData)
+            if (res.success) {
+                setData(res.data ?? [])
+                setTotalPages(res?.totalPages ?? 0)
+            } else {
+                setData([])
+                setTotalPages(0)
+                toast.error("Something went wrong")
+            }
+        } catch (error) {
+            toast.error("Failed to fetch data")
+        } finally {
+            dispatch(setLoading(false))
+        }
     }
 
     useEffect(() => {
-        getDataHandler()
-    }, [start, type])
+        getDataHandler(undefined, 'h_24', null, null)
+    }, [])
 
     const handleNext = () => {
         if (currentPage < totalPages) {
-            getDataHandler(currentPage + 1)
+            setCurrentPage(currentPage + 1)
+            getDataHandler(currentPage + 1, dataType, startDate, endDate)
         }
     }
 
     const handlePrev = () => {
         if (currentPage > 1) {
-            getDataHandler(currentPage - 1)
+            setCurrentPage(currentPage - 1)
+            getDataHandler(currentPage - 1, dataType, startDate, endDate)
         }
     }
 
     const handlePageChange = (page) => {
         if (page > 0 && page <= totalPages) {
-            getDataHandler(page)
+            setCurrentPage(page)
+            getDataHandler(page, dataType, startDate, endDate)
         }
     }
 
@@ -95,19 +103,18 @@ export default function Payout({ direction, ...args }) {
                                 <th>Gross Profit</th>
                             </tr>
                         </thead>
-                        <tbody style={{ fontSize: "13px" }}>
-                            {data?.data?.map((user, index) => {
-                                return (
-                                    <tr key={index}>
-                                        <td>{user?.userId ?? '-'}</td>
-                                        <td>{user?.totalUserAmount ? `${user?.totalUserAmount} ${user.userCurrency}` : '-'}</td>
-                                        <td>{user?.totalUserAmount ? `${user?.totalUserAmount} ${user.userCurrency}` : '-'}</td>
-                                        <td>{user?.totalUserAmount ? `${user?.totalUserAmount} ${user.userCurrency}` : '-'}</td>
-                                        <td>{user?.totalUserAmount ? `${user?.totalUserAmount} ${user.userCurrency}` : '-'}</td>
-                                    </tr>
-                                )
-                            })
-                            }
+                        <tbody style={{fontSize: "13px"}}>
+                        {[...data]
+                            .sort((a, b) => new Date(a.date) - new Date(b.date)) // Descending (latest first)
+                            .map((revenueData, index) => (
+                                <tr key={index}>
+                                    <td>{revenueData?.date ?? '-'}</td>
+                                    <td>{revenueData?.tokenSale ?? '-'}</td>
+                                    <td>{revenueData?.revenue ?? '-'}</td>
+                                    <td>{revenueData?.payouts ?? '-'}</td>
+                                    <td>{revenueData?.grossProfit ?? '-'}</td>
+                                </tr>
+                            ))}
                         </tbody>
                     </Table>
                     <div className="d-flex justify-content-center mt-3">
